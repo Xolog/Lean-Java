be112a8d7b5cfc055f39dc5b33cdaced94948536
xolog <stalk200213@gmail.com>
2021-02-20 18:28:26 +0300

    Готов класс GameLauncher.java

--- src/com/company/GameLauncher.java
+++ src/com/company/GameLauncher.java
@@ -3,6 +3,7 @@ package com.company;
 public class GameLauncher {
 
     public static void main(String[] args) {
-	// write your code here
+        GuessGame game = new GuessGame();
+        game.startGame();
     }
 }
